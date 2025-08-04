e57d9cdfe96cd0d81c2b453bffdc36c410f7a925
Bruno de Fraga <brunofragaa97@gmail.com>
2025-08-04 10:56:01 -0300

    Reparo em erro de compilacao da classe de exception do produto

--- src/main/java/com/restaurante01/api_restaurante/excepetions/PrecoProdutoNegativoException.java
+++ src/main/java/com/restaurante01/api_restaurante/excepetions/PrecoProdutoNegativoException.java
@@ -1,8 +1,4 @@
-<<<<<<<< Updated upstream:src/main/java/com/restaurante01/api_restaurante/excepetions/PrecoProdutoNegativoException.java
 package com.restaurante01.api_restaurante.excepetions;
-========
-package com.restaurante01.api_restaurante.produto.exceptions;
->>>>>>>> Stashed changes:src/main/java/com/restaurante01/api_restaurante/produto/exceptions/PrecoProdutoNegativoException.java
 
 public class PrecoProdutoNegativoException extends RuntimeException {
     public PrecoProdutoNegativoException(String mensagem) {
--- src/main/java/com/restaurante01/api_restaurante/excepetions/ProdutoPossuiHistorico.java
+++ src/main/java/com/restaurante01/api_restaurante/excepetions/ProdutoPossuiHistorico.java
@@ -1,11 +1,7 @@
-<<<<<<<< Updated upstream:src/main/java/com/restaurante01/api_restaurante/excepetions/ProdutoPossuiHistorico.java
 package com.restaurante01.api_restaurante.excepetions;
-========
-package com.restaurante01.api_restaurante.produto.exceptions;
->>>>>>>> Stashed changes:src/main/java/com/restaurante01/api_restaurante/produto/exceptions/ProdutoPossuiHistorico.java
 
-public class ProdutoPossuiHistorico extends RuntimeException{
-    public ProdutoPossuiHistorico(String message){
+public class ProdutoPossuiHistorico extends RuntimeException {
+    public ProdutoPossuiHistorico(String message) {
         super(message);
     }
 }
