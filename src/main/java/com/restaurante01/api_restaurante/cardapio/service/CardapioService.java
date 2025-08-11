b322900bc549c9ee2c7f0c19c1efcd89df167768
Bruno de Fraga <brunofragaa97@gmail.com>
2025-08-11 17:08:49 -0300

    Criado a camada de service e estruturado sua base

---
+++ src/main/java/com/restaurante01/api_restaurante/cardapio/service/CardapioService.java
@@ -0,0 +1,23 @@
+package com.restaurante01.api_restaurante.cardapio.service;
+
+import com.restaurante01.api_restaurante.cardapio.entity.Cardapio;
+import com.restaurante01.api_restaurante.cardapio.repository.CardapioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CardapioService {
+    private final CardapioRepository cardapioRepository;
+
+    @Autowired
+    public CardapioService(CardapioRepository cardapioRepository){
+        this.cardapioRepository = cardapioRepository;
+    }
+    List<Cardapio> listarCardapios(){
+        return cardapioRepository.findAll();
+    }
+
+
+}
