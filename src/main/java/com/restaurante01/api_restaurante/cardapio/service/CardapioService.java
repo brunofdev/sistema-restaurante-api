a5331400eddedf6c789c3dcbcb736c43daec45f5
Bruno de Fraga <brunofragaa97@gmail.com>
2025-08-12 09:35:22 -0300

    removido imports desnecessario

--- src/main/java/com/restaurante01/api_restaurante/cardapio/service/CardapioService.java
+++ src/main/java/com/restaurante01/api_restaurante/cardapio/service/CardapioService.java
@@ -1,12 +1,10 @@
 package com.restaurante01.api_restaurante.cardapio.service;
 
 import com.restaurante01.api_restaurante.cardapio.dto.CardapioDTO;
-import com.restaurante01.api_restaurante.cardapio.entity.Cardapio;
 import com.restaurante01.api_restaurante.cardapio.mapper.CardapioMapper;
 import com.restaurante01.api_restaurante.cardapio.repository.CardapioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
