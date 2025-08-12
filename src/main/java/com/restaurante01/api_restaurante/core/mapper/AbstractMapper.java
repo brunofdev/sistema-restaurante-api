3d9ce60ba4cc63a77894788b019a226d4040879d
Bruno de Fraga <brunofragaa97@gmail.com>
2025-08-12 14:33:57 -0300

    Criado interface mapper e classe abstrata para futuras implementacoes e aplicacao de conceitos de polimorfismo, pensando na manutencao futura

---
+++ src/main/java/com/restaurante01/api_restaurante/core/mapper/AbstractMapper.java
@@ -0,0 +1,17 @@
+package com.restaurante01.api_restaurante.core.mapper;
+
+import java.util.List;
+
+public abstract class AbstractMapper<Entity, DTO> implements Mapper<Entity, DTO> {
+    @Override
+    public List<DTO> mapearLoteParaDTO(List<Entity> entities){
+        return entities.stream()
+                .map(this::mapearEntityParaDTO)
+                .toList();
+    }
+
+    @Override
+    public abstract DTO mapearEntityParaDTO (Entity entity);
+
+}
+
---
+++ src/main/java/com/restaurante01/api_restaurante/core/mapper/Mapper.java
@@ -0,0 +1,15 @@
+package com.restaurante01.api_restaurante.core.mapper;
+
+import java.util.List;
+/**
+*@param <Entity> Tipo da entidade
+*@param <DTO> Tipo do DTO
+*Esta implementação fornece as exigencias
+*necessarias para a menor implementação correta de mappers
+*/
+public interface Mapper <Entity,DTO>{
+    DTO mapearEntityParaDTO(Entity entity);
+    List<DTO> mapearLoteParaDTO(List<Entity> loteEntitys);
+
+
+}
