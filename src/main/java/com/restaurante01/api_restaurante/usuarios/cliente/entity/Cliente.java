258e3cb2c84d8c9e706ccd552945d03e36e616c0
Bruno de Fraga <brunofragaa97@gmail.com>
2025-12-09 15:10:17 -0300

    implementacao de metodo que calcula pontuacao do cliente

--- src/main/java/com/restaurante01/api_restaurante/usuarios/cliente/entity/Cliente.java
+++ src/main/java/com/restaurante01/api_restaurante/usuarios/cliente/entity/Cliente.java
@@ -1,12 +1,15 @@
 package com.restaurante01.api_restaurante.usuarios.cliente.entity;
 
 import com.restaurante01.api_restaurante.usuarios.usuario_super.Usuario;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -15,5 +18,30 @@ import lombok.NoArgsConstructor;
 @Table(name = "clientes")
 @Entity
 public class Cliente extends Usuario {
-    private Long pontuacaoFidelidade;
+    @Column(nullable = false)
+    private int pontuacaoFidelidade;
+
+    private static final BigDecimal LIMITE_FAIXA_BRONZE = BigDecimal.valueOf(50);
+    private static final BigDecimal LIMITE_FAIXA_PRATA = BigDecimal.valueOf(90);
+
+    private static final int PONTOS_BRONZE = 1;
+    private static final int PONTOS_PRATA = 2;
+    private static final int PONTOS_OURO = 3;
+
+
+    //ajustar uma logica mais detalhada no futuro, mas é uma ideia legal
+    public void acrescentarPontuacao(BigDecimal totalPedido){
+        setPontuacaoFidelidade(getPontuacaoFidelidade() + calcularPontuacao(totalPedido));
+    }
+    private int calcularPontuacao(BigDecimal totalPedido) {
+        if (totalPedido == null) {
+            return 0;
+        }
+        if (totalPedido.compareTo(LIMITE_FAIXA_BRONZE) <= 0) {
+            return PONTOS_BRONZE;
+        }else if (totalPedido.compareTo(LIMITE_FAIXA_PRATA) <= 0) {
+            return PONTOS_PRATA;
+        }
+        return PONTOS_OURO;
+    }
 }
