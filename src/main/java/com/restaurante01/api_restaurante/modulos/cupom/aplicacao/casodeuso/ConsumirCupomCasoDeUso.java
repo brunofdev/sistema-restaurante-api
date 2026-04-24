c918cb9bf46c8ff0ba2eaf78409c1c322a6a1e8e
Bruno Fraga <brunofragaa97@gmail.com>
2026-04-24 12:43:41 -0300

    adicionado anotacao para transação independente

--- src/main/java/com/restaurante01/api_restaurante/modulos/cupom/aplicacao/casodeuso/ConsumirCupomCasoDeUso.java
+++ src/main/java/com/restaurante01/api_restaurante/modulos/cupom/aplicacao/casodeuso/ConsumirCupomCasoDeUso.java
@@ -5,8 +5,11 @@ import com.restaurante01.api_restaurante.modulos.cupom.dominio.entidade.Cupom;
 import com.restaurante01.api_restaurante.modulos.cupom.dominio.repositorio.CupomRepositorio;
 import com.restaurante01.api_restaurante.modulos.pedido.dominio.valorobjeto.CupomUtilizado;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
-@Component
+@Service
 public class ConsumirCupomCasoDeUso {
 
     private final CupomRepositorio repositorio;
@@ -15,6 +18,7 @@ public class ConsumirCupomCasoDeUso {
         this.repositorio = repositorio;
     }
 
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public Cupom executar (CodigoCupom codigoCupom){
         Cupom cupom = repositorio.obterPorCodigo(codigoCupom);
         cupom.subtrairQuantidade();
